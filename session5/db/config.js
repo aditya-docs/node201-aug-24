7e0413ba30c3efe0fd46b2e3bf1c079d09640381
Aditya Singh <aditya@tray.io>
2024-08-06 17:57:20 +0100

    updated db name

--- session5/db/config.js
+++ session5/db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGODB_URI = "mongodb://127.0.0.1:27017";
+const MONGODB_URI = "mongodb://127.0.0.1:27017/website";
 
 const connectDB = async () => {
   try {
